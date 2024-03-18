e03db060b3469c0f9428f87009084f5d76f27a3c
mhuang66 <mhuang66@calpoly.edu>
2024-03-17 22:02:16 -0700

    reorganized code

--- src/sketch.js
+++ src/sketch.js
@@ -35,11 +35,11 @@ function draw() {
 
     trash(400, 325);
 
-    ellipse(375, 287.5, 10);
+    // ellipse(375, 287.5, 10);
 
-    ellipse(425, 362.5, 10);
+    // ellipse(425, 362.5, 10);
 
-    ellipse(350, 312.5, 10);
+    // ellipse(350, 312.5, 10);
 
     // scene elements go above this
     leftArrow(50, 200, 0.5);
@@ -83,71 +83,6 @@ function rightArrow(x, y, s) {
   fill(arrowColorR);
   rect(-25, -25, 50, 50);
   triangle(-75, 0, -25, -50, -25, 50);
-  pop();
-}
-
-function tv(x, y, screenColor) {
-  push();
-  noStroke();
-  translate(x, y)
-
-  // tv screen color
-  if (tvInter) {
-    screenColor = color(20, random(100, 150), 200);
-  } else {
-    screenColor = color(0);
-  }
-  fill(screenColor)
-  rect(-75, -75, 150);
-
-  // tv frame
-  if (scene == "center" && 125 < mouseX && mouseX < 275 && 125 < mouseY && mouseY < 275) {
-    frameColor = 75;
-  } else {
-    frameColor = 50;
-  }
-  fill(frameColor);
-  rect(-75, -75, 25, 150);
-  rect(50, -75, 25, 150);
-  rect(-50, -75, 100, 25);
-  rect(-50, 50, 100, 25);
-
-  // table
-  fill(115, 65, 5);
-  rect(-100, 75, 200, 50);
-
-  pop();
-}
-
-function trash(x, y) {
-  push();
-  translate(x, y);
-  noStroke();
-
-  let traColor = 200;
-  if (scene == "center" && traInter == false && 375 < mouseX && mouseX < 425 && 287.5 < mouseY && mouseY < 362.5) {
-    traColor = (230);
-  }
-  if (scene == "center" && traInter == true && 350 < mouseX && mouseX < 425 && 312.5 < mouseY && mouseY < 362.5) {
-    traColor = (230);
-  }
-
-  fill(traColor);
-  if (traInter == true) {
-    rect(-50, -12.5, 75, 50);
-  } else {
-    rect(-25, -37.5, 50, 75);
-  }
-
-  pop();
-}
-
-function centerBg() {
-  push();
-  background(140, 234, 237);
-  noStroke();
-  fill(255, 245, 166);
-  rect(0, 310, width, height);
   pop();
 }
 
@@ -219,4 +154,67 @@ function mouseClicked() {
   }
 }
 
+function tv(x, y, screenColor) {
+  push();
+  noStroke();
+  translate(x, y)
 
+  // tv screen color
+  if (tvInter) {
+    screenColor = color(20, random(100, 150), 200);
+  } else {
+    screenColor = color(0);
+  }
+  fill(screenColor)
+  rect(-75, -75, 150);
+
+  // tv frame
+  if (scene == "center" && 125 < mouseX && mouseX < 275 && 125 < mouseY && mouseY < 275) {
+    frameColor = 75;
+  } else {
+    frameColor = 50;
+  }
+  fill(frameColor);
+  rect(-75, -75, 25, 150);
+  rect(50, -75, 25, 150);
+  rect(-50, -75, 100, 25);
+  rect(-50, 50, 100, 25);
+
+  // table
+  fill(115, 65, 5);
+  rect(-100, 75, 200, 50);
+
+  pop();
+}
+
+function trash(x, y) {
+  push();
+  translate(x, y);
+  noStroke();
+
+  let traColor = 200;
+  if (scene == "center" && traInter == false && 375 < mouseX && mouseX < 425 && 287.5 < mouseY && mouseY < 362.5) {
+    traColor = (230);
+  }
+  if (scene == "center" && traInter == true && 350 < mouseX && mouseX < 425 && 312.5 < mouseY && mouseY < 362.5) {
+    traColor = (230);
+  }
+
+  fill(traColor);
+  if (traInter == true) {
+    rect(-50, -12.5, 75, 50);
+  } else {
+    rect(-25, -37.5, 50, 75);
+  }
+
+  pop();
+}
+
+function centerBg() {
+  push();
+  background(140, 234, 237);
+  noStroke();
+  fill(255, 245, 166);
+  rect(0, 310, width, height);
+  pop();
+}
